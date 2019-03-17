e84ca93b2486812fdf753dc3b8d8db7c2b12acce
LandiMi2 <landycofia@gmail.com>
2019-03-17 19:48:49 +0300

    Added a script

---
+++ Scripts/bank_Mike_final.py
@@ -0,0 +1,31 @@
+#!/home/cofia/miniconda3/bin/python
+
+acountbal = 50000
+choice = input("Please enter 'b' to check balance or 'w' to withdraw or deposit 'd': ")
+while choice != 'q':
+    if choice.lower() in ('w','b','d'):
+        if choice.lower() == 'b':
+            print("Your balance is: %d" % acountbal)
+            print("Anything else?")
+            choice = input("Enter b for balance, w to withdraw or q to quit: ")
+            print(choice.lower())
+        elif choice.lower() == 'd' :
+                deposit = float(input("Enter amount to deposit:"))
+                if deposit > 0:
+                    print("This is the  amount you have deposited:%d" %deposit)
+                    acountbal = acountbal + deposit
+                    print("Anything else?")
+                    choice = input("Enter b for balance, w to withdraw or q to quit: ")
+        else:
+            withdraw = float(input("Enter amount to withdraw: "))
+            if withdraw <= acountbal:
+                print("here is your: %.2f" % withdraw)
+                acountbal = acountbal - withdraw
+                print("Anything else?")
+                choice = input("Enter b for balance, w to withdraw or q to quit: ")
+                
+            else:
+                print("You have insufficient funds: %.2f" % acountbal)
+    else:
+        print("Wrong choice!")
+        choice = input("Please enter 'b' to check balance or 'w' to withdraw: ")
